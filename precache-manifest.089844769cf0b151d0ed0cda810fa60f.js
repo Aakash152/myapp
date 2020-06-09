158f7df6ae6f0858103646f4c36ff172a67e68d1
Aakash152 <aakashkharche152@gmail.com>
2020-06-09 18:41:04 +0530

    Updates

---
+++ precache-manifest.089844769cf0b151d0ed0cda810fa60f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98a27caafc7a2b058e33f21d63f74a5",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b0ebd0f62e069721db6d",
+    "url": "/myapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37a7751e211e3af6bd09",
+    "url": "/myapp/static/js/2.f5a3bdb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.f5a3bdb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0ebd0f62e069721db6d",
+    "url": "/myapp/static/js/main.b84bccc2.chunk.js"
+  },
+  {
+    "revision": "ac2fcaa340cf94fd56fc",
+    "url": "/myapp/static/js/runtime-main.7bbbfcf9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
